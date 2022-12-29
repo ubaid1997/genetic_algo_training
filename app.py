a0f9e12fa25936ac6f2b75c6c959faa3ec3c36cb
ubaid rahim <ubaidrahim1997@gmail.com>
2022-12-29 16:13:04 -0700

    first commit firt commit to be published

--- app.py
+++ app.py
@@ -19,9 +19,19 @@ params = structure()
 params.maxit = 100
 params.npop = 20
 params.pc = 1
+params.gamma = 0.1
+params.sigma = 0.1
+params.mu = 0.1
 
 # Run GA
 out = ga.run(problem,params)
 print(0)
 
 # Results
+plt.semilogy(out.bestcost)
+plt.xlim(0,params.maxit)
+plt.xlabel('Iterations')
+plt.ylabel('Best Cost')
+plt.title('Genetic Algo (GA)')
+plt.grid(True)
+plt.show()
--- ga.py
+++ ga.py
@@ -13,7 +13,11 @@ def run(problem, params):
     maxit = params.maxit
     npop = params.npop
     pc = params.pc
-    nc = pc*npop
+    nc = int(np.round(pc*npop/2)*2)
+    gamma = params.gamma
+    mu = params.mu
+    sigma = params.sigma
+
     # Empty Individual Template
     empty_individual = structure() 
     empty_individual.position = None
@@ -29,9 +33,8 @@ def run(problem, params):
         pop[i].position = np.random.uniform(varmin, varmax, nvar)
         pop[i].cost = costfunc(pop[i].position)
         if pop[i].cost < bestsol.cost:
-            bestsol = pop[i].deepcopy
-            print(bestsol.cost)
-
+            bestsol = pop[i].deepcopy()
+          
     # Best Cost of Iterations
     bestcost = np.empty(maxit)
 
@@ -39,14 +42,73 @@ def run(problem, params):
     for it in range(maxit):
         
         popc = []
-        for k in range(nc//2):
-            popc
+        for _ in range(nc//2):
+            q = np.random.permutation(npop)
+            p1 = pop[q[0]]
+            p2 = pop[q[1]]
+
+            # Perform crossover
+            c1, c2 = crossover(p1,p2, gamma)
+
+            # Mutation Function
+            c1 = mutate(c1, mu, sigma)
+            c2 = mutate(c2, mu, sigma)
+
+            # Apply bounds to mutation
+            apply_bound(c1, varmin, varmax)
+            apply_bound(c2, varmin, varmax)
+
+            #evaluate first offspring
+            c1.cost = costfunc(c1.position)
+            if c1.cost < bestsol.cost:
+                bestsol = c1.deepcopy()
+
+            #evaluate first offspring
+            c2.cost = costfunc(c2.position)
+            if c2.cost < bestsol.cost:
+                bestsol = c2.deepcopy()
+
+            # Add Offspring to population;
+            popc.append(c1)
+            popc.append(c2)
+
+        # Merge, Sort and Select
+        pop += popc
+        pop = sorted(pop, key=lambda x: x.cost)
+        pop = pop[0:npop]
+
+        # Store best cost
+        bestcost[it] = bestsol.cost
+
+        #show iteration info
+        print("Iteration {}: Best Cost = {}".format(it, bestcost[it]))
+
 
     # Output
     out = structure() 
+    out.bestsol = bestsol
+    out.bestcost = bestcost
     out.pop = pop
 
     return out
 
+def crossover(p1,p2,gamma = 0.1):
+    c1 = p1.deepcopy()
+    c2 = p1.deepcopy()
+    
+    alpha = np.random.uniform(-gamma, 1+gamma, *c1.position.shape)
+    c1.position = alpha*p1.position + (1-alpha)*p2.position
+    c2.position = alpha*p2.position + (1-alpha)*p1.position
+    return c1, c2
 
+def mutate(x, mu, sigma):
+    y = x.deepcopy()
+    flag = np.random.rand(*x.position.shape) <= mu 
+    ind = np.argwhere(flag)
+    y.position[ind] = x.position[ind] + sigma*np.random.randn(*ind.shape)
+    return y
+
+def apply_bound(x, varmin,varmax):
+    x.postiion = np.maximum(x.position, varmin)
+    x.postiion = np.minimum(x.position, varmax)
 
